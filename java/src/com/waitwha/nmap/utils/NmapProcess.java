cd791b34c6d70899d4432fa2a1a7e7097ca23372
Mike Duncan <mike.duncan@noaa.gov>
2014-06-03 14:51:29 -0400

    Fixed command generation for ProcessBuilder

--- java/src/com/waitwha/nmap/utils/NmapProcess.java
+++ java/src/com/waitwha/nmap/utils/NmapProcess.java
@@ -3,6 +3,8 @@ package com.waitwha.nmap.utils;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.logging.Logger;
 
 import com.waitwha.logging.LogManager;
@@ -83,8 +85,15 @@ public final class NmapProcess
 		
 		try  {
 			log.info(String.format("Starting %s process with arguments: %s", this.path, ArrayUtils.merge(this.args, " ")));
+			
+			List<String> commands = new ArrayList<String>();
+			commands.add(this.path.toString());
+			for(String arg : this.args)
+				commands.add(arg);
+			
 			ProcessBuilder builder = 
-					new ProcessBuilder(this.path.toString(), ArrayUtils.merge(this.args, " "));
+					new ProcessBuilder(commands);
+			builder.directory(new File(System.getProperty("user.dir")));
 			builder.redirectErrorStream(true);
 			Process p = builder.start();
 			BufferedReader procReader = 
